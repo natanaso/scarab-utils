8a6e59e50322f4cb7bc27d2c561e98dfd5b0a587
Ben Charrow <bcharrow@seas.upenn.edu>
2010-09-30 09:54:11 -0400

    Upgrade ros-setup.sh to use cturtle

--- ros-setup.sh
+++ ros-setup.sh
@@ -1,4 +1,4 @@
-source /opt/ros/setup.sh
+source /opt/ros/cturtle/setup.sh
 # List all folders found in $HOME/git & $HOME/svn
 PACKAGES=`find $HOME/git $HOME/svn -maxdepth 1 -mindepth 1 | sed 's/$/:/' | tr -d '\n'`
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${PACKAGES}
