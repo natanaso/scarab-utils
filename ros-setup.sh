b20f9acc6617c27f60e28e24cbef30c002440049
Ben Charrow <bcharrow@seas.upenn.edu>
2011-02-03 09:30:05 -0500

    Set fully qualified names for ROS_MASTER_URI and ROS_IP

--- ros-setup.sh
+++ ros-setup.sh
@@ -6,8 +6,12 @@ else
     echo "Problem finding ros setup file"
 fi
 
+
+export ROS_IP=$(hostname -f)
+export ROS_MASTER_URI=http://${ROS_IP}:11311
+
 # List all folders found in $HOME/git & $HOME/svn
 PACKAGES=`find $HOME/git $HOME/svn -maxdepth 1 -mindepth 1 | sed 's/$/:/' | tr -d '\n'`
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${PACKAGES}
 unset PACKAGES
-export ROS_PACKAGE_PATH=$HOME/git/bearing-localization-with-rssi:$HOME/git/multimaster-ros-pkg:${ROS_PACKAGE_PATH}
+export ROS_PACKAGE_PATH=$HOME/git/scarab:$HOME/git/multimaster-ros-pkg:${ROS_PACKAGE_PATH}
